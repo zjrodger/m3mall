26dec4b7209a4f6515d7dfde46cc4a2f9d8eda29
zjrodger <zjrodger@163.com>
2018-01-10 19:19:29 +0800

    增加相关的分页插件

--- e3-manager/e3-manager-service/src/main/java/com/zjrodger/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/com/zjrodger/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package com.zjrodger.e3mall.service.impl;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zjrodger.e3mall.common.pojo.EasyUIDataGridResult;
 import com.zjrodger.e3mall.mapper.TbItemMapper;
 import com.zjrodger.e3mall.pojo.TbItem;
+import com.zjrodger.e3mall.pojo.TbItemExample;
 import com.zjrodger.e3mall.service.ItemService;
 
 /**
@@ -17,11 +24,28 @@ import com.zjrodger.e3mall.service.ItemService;
 public class ItemServiceImpl implements ItemService {
 	
 	@Autowired
-	private TbItemMapper tbItemMapper;
+	private TbItemMapper itemMapper;
 
 	@Override
 	public TbItem getItemById(long id) {
-		return tbItemMapper.selectByPrimaryKey(id);
+		return itemMapper.selectByPrimaryKey(id);
 	}
 
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//创建一个返回值对象
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setRows(list);
+		//取分页结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//取总记录数
+		long total = pageInfo.getTotal();
+		result.setTotal(total);
+		return result;
+	}	
+	
 }
